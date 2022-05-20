7a7606b5392d9ec44ce34a545f5f649a8fe69bfb
Vincent Knoop Pathuis <git@vpathuis.dds.nl>
2022-05-20 23:41:49 +0200

    fixed documation in module

--- uh50_api/uh50.py
+++ uh50_api/uh50.py
@@ -2,9 +2,6 @@
 This module reads from the Landys & Gyr UH50 heat measuring unit and returns the currect GJ and m3 meters.
 WARNING: everytime this is called, battery time of the UH50 will go down by about 30 minutes!
 
-Usage: read_uh50(port), eg read_uh50('/dev/ttyUSB0') or read_uh50('COM5')
-
-Calling the module directly will allow for usage through command lime
 """
 from datetime import datetime
 from pyexpat import model
