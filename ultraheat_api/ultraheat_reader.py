1cdec0ee4401bc347fb0efae13f8d73c5ac7a73c
Vincent Knoop Pathuis <48653141+vpathuis@users.noreply.github.com>
2022-09-08 09:49:26 +0200

    Add debug logging (#5)

--- ultraheat_api/ultraheat_reader.py
+++ ultraheat_api/ultraheat_reader.py
@@ -2,20 +2,24 @@
 Reads raw response data from the Ultraheat unit. 
 To test the connection use validate, which will return the model name.
 """
+import logging
 import serial
 from serial import Serial
 
+_LOGGER = logging.getLogger(__name__)
+
 MAX_LINES_ULTRAHEAT_REPONSE = 26
+
 
 class UltraheatReader:
     def __init__(self, port) -> None:
+        _LOGGER.debug("Initializing UltraheatReader on port: %s", port)
         self._port = port
 
-    def read(self) -> str:
+    def read(self):
         "Reads the device on the specified port, returning the full string"
         with self._connect_serial() as conn:
             return self._get_data(conn)
-
 
     def _connect_serial(self) -> Serial:
         "Make the connection to the serial device"
@@ -34,6 +38,7 @@ class UltraheatReader:
         "Wake up the device and get the model number"
         # Waking up should be done at 300 baud
         # Sending /?!
+        _LOGGER.debug("Waking up Ultraheat")
         conn.write(
             b"\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x2F\x3F\x21\x0D\x0A"
         )
@@ -44,12 +49,16 @@ class UltraheatReader:
 
         # checking if we can read the model (eg. 'LUGCUH50')
         model = conn.readline().decode("utf-8")[1:9]
+        if model:
+            _LOGGER.debug("Got model %s", model)
         if not (model):
+            _LOGGER.error("No model could be read")
             raise Exception("No model could be read")
         return model
 
     def _get_data(self, conn):
         model = self._wake_up(conn)
+        _LOGGER.debug("Receiving data")
         # Now switch to 2400 BAUD. This could be different for other models. Let me know if you experience problems.
         conn.baudrate = 2400
         ir_lines = ""
@@ -61,4 +70,5 @@ class UltraheatReader:
             ir_line = conn.readline()
             ir_lines += ir_line.decode("utf-8")
 
+        _LOGGER.debug("Read %s lines of data", iteration)
         return (model, str(ir_lines))
